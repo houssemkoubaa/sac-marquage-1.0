40ee0afa56450e35a09dd129e43c544371c610b6
houssem <houssem.koubaa2000@gmail.com>
2023-01-30 16:20:21 +0100

    PRIVATE_MCHET

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,37 +1,32 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
-import {
-  HashRouter as Router,
-  Route,
-  Routes as Switch,
-  Redirect,
-} from "react-router-dom";
-
-import { Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 import Alerts from "./layout/Alerts";
-import Login from "./accounts/Login";
-import Register from "./accounts/Register";
-import PrivateRoute from "./commun/PrivateRoute";
-
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  redirect,
+  Routes,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../store";
-import { loadUser } from "../actions/auth";
-import { loadUser } from "../actions/auth";
-// Alert Options
+import { Login } from "./accounts/Login";
+import Register from "./accounts/Login";
+// connect redux to react is through the provider , we need to wrap around it everything
+
+import PrivateRoute from "./commun/PrivateRoute";
+
+//Alert options is an object
 const alertOptions = {
   timeout: 3000,
   position: "top center",
 };
 
 class App extends Component {
-  componentDidMount() {
-    store.dispatch(loadUser());
-  }
-
   render() {
     return (
       <Provider store={store}>
@@ -41,12 +36,13 @@ class App extends Component {
               <Header />
               <Alerts />
               <div className="container">
-                <Switch>
-                  {/* <PrivateRoute exact path="/" element={<Dashboard />} /> */}
-                  <Route exact path="/" element={<Dashboard />} />
-                  <Route exact path="/register" element={<Register />} />
-                  <Route exact path="/login" element={<Login />} />
-                </Switch>
+                <Routes>
+                  <Route exact path="/" element={<PrivateRoute />}>
+                    <Route exact path="/" element={<Dashboard />} />
+                  </Route>
+                  <Route path={"/register"} element={<Register />}></Route>
+                  <Route path={"/login"} element={<Login />}></Route>
+                </Routes>
               </div>
             </Fragment>
           </Router>
@@ -57,3 +53,9 @@ class App extends Component {
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+// the provider takes in the store as a prop
+
+// --watch in package.json in dev is for when u change smth in ur app or components , you don't have
+// to do npm run dev again each time to reload/update main.js , --watch will take care of it
+// and u ll just have to ctrl+r
--- leadmanager/frontend/src/components/commun/PrivateRoute.js
+++ leadmanager/frontend/src/components/commun/PrivateRoute.js
@@ -1,25 +1,41 @@
-import React, { Children } from "react";
-import { Route, Navigate } from "react-router-dom";
+// import React, { Children } from "react";
+// import { Route, Navigate } from "react-router-dom";
+// import { connect } from "react-redux";
+// import PropTypes from "prop-types";
+
+// const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) => {
+//       if (auth.isLoading) {
+//         return <h2>Loading...</h2>;
+//       } else if (!auth.isAuthenticated) {
+//         return <Navigate to="/login" />;
+//       } else {
+//         return <Component {...props} />;
+//       }
+//     }}
+//   />
+// );
+
+// const mapStateToProps = (state) => ({
+//   auth: state.auth,
+// });
+
+// export default connect(mapStateToProps)(PrivateRoute);
+import React from "react";
+import { Route, Navigate, Outlet } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      if (auth.isLoading) {
-        return <h2>Loading...</h2>;
-      } else if (!auth.isAuthenticated) {
-        return <Navigate to="/login" />;
-      } else {
-        return <Component {...props} />;
-      }
-    }}
-  />
-);
+const useAuth = () => {
+  const user = { loggedIn: false };
+  return user && user.loggedIn;
+};
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
+const PrivateRoute = () => {
+  const isAuth = useAuth();
+  return isAuth ? <Outlet /> : <Navigate to="/login" />;
+};
 
-export default connect(mapStateToProps)(PrivateRoute);
+export default PrivateRoute;
